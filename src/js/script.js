44fd1534535591f354b4ad20ac440ccf87e550b6
Vova <keklolorbedol13@gmail.com>
2022-09-23 21:56:36 +0300

    catalog added

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,49 @@
-    window.addEventListener('DOMContentLoaded', () => {
-        const menu = document.querySelector('.menu'),
-            menuItem = document.querySelectorAll('.menu_item'),
-            hamburger = document.querySelector('.hamburger');
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+        dots: false,
+        speed: 300,
+        slidesToShow: 1,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrow.svg"></button>',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: false,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
-        hamburger.addEventListener('click', () => {
-            hamburger.classList.toggle('hamburger_active');
-            menu.classList.toggle('menu_active');
-        });
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass
+        ('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
 
-        menuItem.forEach(item => {
-            item.addEventListener('click', () => {
-                hamburger.classList.toggle('hamburger_active');
-                menu.classList.toggle('menu_active');
+    function toggleSlide(item) {
+        $(item).each(function (i) {
+            $(this).on('click', function (e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
             })
         })
-    })
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+});
+
